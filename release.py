b4f6006d78ebfa4745cd70f0dc0a2cec284231ef
RealStjimmy <54416124+RealStJimmy@users.noreply.github.com>
2021-07-13 13:30:59 -0500

    Update release.py

--- release.py
+++ release.py
@@ -2,15 +2,11 @@ import schedule
 import time
 from twilio.rest import Client
 
-client = Client('ACaaebe8c489801a9f8f302a0bbc25f1ec', 'f51e489628ba64e7f77af266c023bf8e')
+client = Client('Client Data', 'Client Data')
 
 content = '''
-If you're reciving this, Aiden Dombrosky is dead.\n
-Now don't panic, it's entirely possible he's just really shitty at writing code and that he's still alive.\n 
-Or that he's on vacation and forgot to turn off the system\n
-Go to this site for his last message to you.\n 
-If you know he's alive please don't go here\n
-http://99.108.71.191:443/M2PMavToIbuJutmknngE
+PLACEHOLDER FOR RELASE INFOR 
+Website placeholder 
 '''
 
 
@@ -20,12 +16,8 @@ def releaseall():
     status = file.read()
     print(status) 
     if status == "deaddead": 
-        client.messages.create(to="+12058642045", from_="+12054797696", body=content)
-        client.messages.create(to="+12056009541", from_="+12054797696", body=content)
-        client.messages.create(to="+12055426123", from_="+12054797696", body=content)
-        client.messages.create(to="+12057578890", from_="+12054797696", body=content)
-        client.messages.create(to="+12057197187", from_="+12054797696", body=content)
-        client.messages.create(to="+12058877999", from_="+12054797696", body=content)
+        client.messages.create(to="+NumberPlaceHolder", from_="+NumberPlaceholder", body=content)
+
 
 schedule.every().sunday.at("23:30").do(releaseall)
 
